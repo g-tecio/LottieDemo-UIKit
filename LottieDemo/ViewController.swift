a575359dc50c93a5727f4fb9de5da0127987b8d0
Fernando Vazquez <fdo@cwgx.io>
2018-08-23 00:59:26 -0500

    Initial Commit

---
+++ LottieDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LottieDemo
+//
+//  Created by Fernando Vazquez on 8/23/18.
+//  Copyright © 2018 Cartwheel Galaxy Games. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
